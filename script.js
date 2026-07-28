9b5ed56dedbd51b752bb0164c190b219a3697d95
applemagic-01 <thatuserpc@gmail.com>
2026-07-28 20:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your code here
+const mainImage = document.querySelector(".photo-main > img");
+const variants = document.querySelectorAll(".variant > ul > li");
+
+variants.forEach((variant) => {
+	variant.addEventListener("click", function () {
+		const selectedImage = this.querySelector("img");
+
+		if (selectedImage && mainImage) {
+			mainImage.src = selectedImage.src;
+			mainImage.alt = selectedImage.alt;
+		}
+
+		variants.forEach((item) => {
+			item.classList.remove("selected");
+		});
+
+		this.classList.add("selected");
+	});
+});
+
+const albumImages = document.querySelectorAll(
+	".photo-album > ul > li > img"
+);
+
+albumImages.forEach((image) => {
+	image.addEventListener("click", function () {
+		if (mainImage) {
+			mainImage.src = this.src;
+			mainImage.alt = this.alt;
+		}
+	});
+});
+
+const cartButton = document.querySelector(".buy--btn");
+
+if (cartButton) {
+	cartButton.addEventListener("click", function () {
+		this.textContent = "ADDED TO CART";
+
+		setTimeout(() => {
+			this.textContent = "ADD TO CART";
+		}, 1500);
+	});
+}
